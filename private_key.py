e632492bdc6f87a4ad06b2dd9c8bb613a610a9e1
peardragon <56184514+peardragon@users.noreply.github.com>
2022-01-02 15:50:37 +0900

    Update private_key.py

--- private_key.py
+++ private_key.py
@@ -3,5 +3,5 @@ db_ip = "127.0.0.1"
 db_passwd = "qowodyd12!"
 db_port = "3306"
 
-upbit_access = "qQwsP4PoPvSLfJLTu3wH6LfoXb2ju7mLxcIW6ZHg"
-upbit_secret = "cBvMiiYfpx7HwL1WQ8kHwEFrWV9JD36Yox1gjDiv"
+upbit_access = "YOURACCESCODE"
+upbit_secret = "YOURSECRETCODE"
